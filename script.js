212a76ffdc0f24c05a55079da0e337d9606fc6a0
IllaMelo <dantas.mill.melo@gmail.com>
2022-03-08 09:19:00 -0300

    Calc

--- script.js
+++ script.js
@@ -8,43 +8,15 @@
   
   function element() {
     if (this.value == "reset") {
-      input.value = "";
+      input.innerHTML = "";
     } else if (this.value == "del") {
-      input.value = input.value.slice(0, -1);
+      input.innerHTML= input.innerHTML.slice(0, -1);
     }
     else if(this.value == "equity"){
-        for(var i = 0; i< input.value.length; i++){
-    
-            if (input.value[i] == "+"){
-            newVal = input.value.split("+");
-            var x = parseFloat(newVal[0]);
-            var y = parseFloat(newVal[1]);
-            input.value = x + y;
-         }
-         else if (input.value[i] == "-"){
-            newVal = input.value.split("-");
-            var x = parseFloat(newVal[0]);
-            var y = parseFloat(newVal[1]);
-            input.value = x - y; 
-         }
-         else if (input.value[i] == "/"){
-            newVal = input.value.split("/");
-            var x = parseFloat(newVal[0]);
-            var y = parseFloat(newVal[1]);
-            input.value = x / y;
-         }
-         else if (input.value[i] == "x"){
-            newVal = input.value.split("x");
-            var x = parseFloat(newVal[0]);
-            var y = parseFloat(newVal[1]);
-            input.value = x * y;
-         }
-        }
-
-    }
-    else {
-      input.value += this.value;
-    }
-  }
+        input.innerHTML = eval(input.innerHTML)}
+    else{
+      input.innerHTML += this.value;
+    }}
+  
   
 })();
